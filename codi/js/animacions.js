a2071a8bde581c5074789ab83ac9c20f570645eb
Sailok <albaayvi4@gmail.com>
2025-02-20 16:20:34 +0100

    ryu implementado

--- codi/js/animacions.js
+++ codi/js/animacions.js
@@ -1,4 +1,11 @@
 // Animacions Player2
+let ready1 = [
+    {x: 6, y: 14, width: 60,height: 91},
+    {x: 74, y: 13, width: 60,height: 91},
+    {x: 141, y: 13, width: 61,height: 90},
+    {x: 210, y: 10, width: 55,height: 93}
+];
+
 let ready2 = [
     {x: 3, y: 528, width: 59,height: 90},
     {x: 69, y: 529, width: 59,height: 89},
--- codi/js/sf2.js
+++ codi/js/sf2.js
@@ -1,7 +1,83 @@
 //variables globals
 var canvas, ctx, interval;
 
-//clase lluitador
+//clase player1
+let player1 = function (x, y, width, height, imatge){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.imatge = imatge;
+
+    this.velocitat = 3;
+    this.dreta = true;
+    this.frameContador = 0;
+    this.frameDelay = 5;
+
+    this.dibuixa = function(){
+        sprite = new Image();
+        sprite.src = this.imatge;
+        ctx.drawImage(sprite,
+                      this.canvas_x,
+                      this.canvas_y,
+                      this.canvas_w,
+                      this.canvas_h,
+                      this.x,
+                      this.y,
+                      this.width,
+                      this.height
+                    );
+    }
+
+    this.mou = function(){
+        if(this.dreta == true){
+            //codi dreta
+            if(this.x < 770){
+                this.x += this.velocitat;
+            }else{
+                this.dreta = false;
+            }
+        }else{
+            //codi esquerra
+            if(this.x > 0){
+                this.x -= this.velocitat;
+            }else{
+                this.dreta = true;
+            }   
+        }
+    }
+
+    this.esquerra = function(){
+        this.x -= this.velocitat;
+    }
+
+    this.dreta = function(){
+        this.x += this.velocitat;
+    }
+
+    let currentFrame = 0;
+    this.animacio = function(){
+        if(this.frameContador >= this.frameDelay){
+            //codi animacio
+            currentFrame = (currentFrame + 1) % ready1.length;
+            frame = ready1[currentFrame];
+
+            this.canvas_x = frame.x;
+            this.canvas_y = frame.y;
+            this.canvas_w = frame.width;
+            this.canvas_h = frame.height;
+            
+            this.frameContador = 0;
+        } else{
+            this.frameContador++;
+        }
+        this.dibuixa();
+        // interval_player1 = requestAnimationFrame(this.animacio.bind(this)); 
+    }
+}
+
+
+//clase player2
 let player2 = function (x, y, width, height, imatge){
     this.x = x;
     this.y = y;
@@ -76,33 +152,33 @@ let player2 = function (x, y, width, height, imatge){
     }
 }
 
+
 // creem el jugador 1 y 2
+let ryu = new player1 (70, 99, 59, 90, 'img/ryu_sprite.png'); //x, y, width, height, color = posHoritzontalCanvas, posVerticalCanvas, amplePersonatge, alturaPersonatge, imgPersonatge
 let ken = new player2 (300, 100, 59, 90, 'img/ken_sprite.png'); //x, y, width, height, color = posHoritzontalCanvas, posVerticalCanvas, amplePersonatge, alturaPersonatge, imgPersonatge
 
 
 //events de teclat
 document.addEventListener('keydown', (e)=>{
 
-//     // Tecles Jugador 1
-//     if(e.key == 'w'){
-//         console.log('jugador 1 - amunt');
-//         player1.amunt();
-//     }
+    // Tecles Jugador 1
+    if(e.key == 'w'){
+        console.log('jugador 1 - amunt');
+    }
 
-//     if(e.key == 's'){
-//         console.log('jugador 1 - abaix');
-//         player1.abaix();
-//     }
+    if(e.key == 's'){
+        console.log('jugador 1 - abaix');
+    }
 
-//     if(e.key == 'a'){
-//         console.log('jugador 1 - esquerra');
-//         player1.esquerra();
-//     }
+    if(e.key == 'a'){
+        ryu.esquerra();
+        console.log('jugador 1 - esquerra');
+    }
 
-//     if(e.key == 'd'){
-//         console.log('jugador 1 - dreta');
-//         player1.dreta();
-//     }
+    if(e.key == 'd'){
+        ryu.dreta();
+        console.log('jugador 1 - dreta');
+    }
 
     // Tecles Jugador 2
     if(e.key == 'ArrowUp'){
@@ -138,11 +214,10 @@ function principal(){
 
     ken.dibuixa();
     ken.animacio();
-    // player1.mou();
 
-    // player2.dibuixa();
-    // player2.mou();
-    
+    ryu.dibuixa();
+    ryu.animacio();
+        
     interval = requestAnimationFrame(principal);
 }
 
