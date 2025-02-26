12c1f1dc19db26b4aedb1de28534f051239c0985
Sailok <albaayvi4@gmail.com>
2025-02-26 20:23:20 +0100

    ficarf ob segueix pendent

---
+++ codi/js/otros/otros.js
@@ -0,0 +1,97 @@
+// pose de la victoria (ma fent el 2)
+let pose_victoria = [
+    {x: 344, y: 16, width: 16, height: 16} 
+];
+
+
+// lletres amb el K.O.
+let ko = [
+    {x: 161, y: 16, width: 32, height: 14}   
+];
+
+
+// barra de vida groga p1
+let barra_vida_1 = [
+    {x: 16, y: 18, width: 145, height: 11}  
+];
+
+
+// barra de vida groga p2
+let barra_vida_2 = [
+    {x: 193, y: 18, width: 145, height: 11}
+];
+
+
+// Estat de la vida a 0
+let estat_vida = [
+    {x: 0, y: 0, width: 639, height: 480}   
+];
+
+
+let num_1 = [
+    {x: 0, y: 0, width: 639, height: 480}  
+];
+
+
+let num_2 = [
+    {x: 0, y: 0, width: 639, height: 480}
+];
+
+
+let lletra_p = [
+    {x: 0, y: 0, width: 639, height: 480}   
+];
+
+
+
+
+
+let nom_blanka = [
+    {x: 56, y: 56, width: 57, height: 9}   
+    
+];
+
+
+let nom_cammy = [
+    {x: 0, y: 0, width: 639, height: 480}
+];
+
+
+let nom_dee_jay = [
+    {x: 0, y: 0, width: 639, height: 480}   
+];
+
+
+let nom_dhalsim = [
+    {x: 0, y: 0, width: 639, height: 480}   
+];
+
+
+let nom_guile = [
+    {x: 0, y: 0, width: 639, height: 480}  
+];
+
+
+let nom_ken = [
+    {x: 0, y: 0, width: 639, height: 480}
+];
+
+
+let nom_m_bison = [
+    {x: 0, y: 0, width: 639, height: 480}   
+];
+
+
+let nom_ryu = [
+    {x: 16, y: 56, width: 28, height: 9}  
+];
+
+
+let nom_sagat = [
+    {x: 0, y: 0, width: 639, height: 480}  
+];
+
+
+let nom_zangief = [
+    {x: 0, y: 0, width: 639, height: 480}  
+];
--- codi/js/personatges/m_bison.js
+++ codi/js/personatges/m_bison.js
@@ -1,4 +1,3 @@
-
 let m_bison_base = [
     {x: 1, y: 31, width: 104,height: 96},
     {x: 114, y: 33, width: 103,height: 94},
