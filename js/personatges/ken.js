04479e303bcbe9bcdc1276d13c77acecd60a5d94
Sailok <127998789+Sailok25@users.noreply.github.com>
2025-03-27 20:17:17 +0100

    Add files via upload

---
+++ js/personatges/ken.js
@@ -0,0 +1,6 @@
+let ken_base = [
+    {x: 3, y: 528, width: 59,height: 90},
+    {x: 69, y: 529, width: 59,height: 89},
+    {x: 133, y: 526, width: 58,height: 92},
+    {x: 200, y: 522, width: 55,height: 94}
+];
