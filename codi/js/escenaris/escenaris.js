0b3fb4e6ea348e4ee3951c0989b4ec72450a7443
Sailok <127998789+Sailok25@users.noreply.github.com>
2025-03-27 20:12:44 +0100

    Add files via upload

---
+++ codi/js/escenaris/escenaris.js
@@ -0,0 +1,117 @@
+/* img/backgrounds --> bg1 */
+let escenari_campana = [
+    {x: 0, y: 0, width: 639, height: 480},   
+    {x: 639, y: 0, width: 639, height: 480},   
+    {x: 1278, y: 0, width: 639, height: 480},   
+    {x: 1917, y: 0, width: 639, height: 480},   
+    {x: 2556, y: 0, width: 639, height: 480}    
+];
+
+
+
+/* img/backgrounds --> bg2 */
+let escenari_vegas = [
+    {x: 0, y: 0, width: 639, height: 480},   
+    {x: 639, y: 0, width: 639, height: 480},   
+    {x: 1278, y: 0, width: 639, height: 480},   
+    {x: 1917, y: 0, width: 639, height: 480},   
+    {x: 2556, y: 0, width: 639, height: 480},   
+    {x: 3195, y: 0, width: 639, height: 480},   
+    {x: 3834, y: 0, width: 639, height: 480},   
+    {x: 4473, y: 0, width: 639, height: 480},   
+    {x: 5112, y: 0, width: 639, height: 480},   
+    {x: 5751, y: 0, width: 639, height: 480},   
+    {x: 6390, y: 0, width: 639, height: 480},   
+    {x: 7029, y: 0, width: 639, height: 480},   
+    {x: 7668, y: 0, width: 639, height: 480},   
+    {x: 8307, y: 0, width: 639, height: 480},   
+    {x: 8946, y: 0, width: 639, height: 480},   
+    {x: 9585, y: 0, width: 639, height: 480},   
+    {x: 10224, y: 0, width: 639, height: 480},   
+    {x: 10863, y: 0, width: 639, height: 480},   
+    {x: 11502, y: 0, width: 639, height: 480},   
+    {x: 12141, y: 0, width: 639, height: 480},   
+    {x: 12780, y: 0, width: 639, height: 480},   
+    {x: 13419, y: 0, width: 639, height: 480},   
+    {x: 14058, y: 0, width: 639, height: 480},   
+    {x: 14697, y: 0, width: 639, height: 480},   
+    {x: 15336, y: 0, width: 639, height: 480},   
+    {x: 15975, y: 0, width: 639, height: 480},   
+    {x: 16614, y: 0, width: 639, height: 480},   
+    {x: 17253, y: 0, width: 639, height: 480},   
+    {x: 17892, y: 0, width: 639, height: 480},   
+    {x: 18531, y: 0, width: 639, height: 480},   
+    {x: 19170, y: 0, width: 639, height: 480},   
+    {x: 19809, y: 0, width: 639, height: 480}
+];
+
+
+
+
+/* img/backgrounds --> bg2 */
+let escenari_termas = [
+    {x: 0, y: 0, width: 768, height: 224},   
+    {x: 768, y: 0, width: 768, height: 224},   
+    {x: 1536, y: 0, width: 768, height: 224},   
+    {x: 2304, y: 0, width: 768, height: 224},   
+    {x: 3072, y: 0, width: 768, height: 224},   
+    {x: 3840, y: 0, width: 768, height: 224},   
+    {x: 4608, y: 0, width: 768, height: 224}    
+];
+
+
+
+/* img/backgrounds --> bg4 */
+let escenari_templo = [
+    {x: 0, y: 0, width: 1280, height: 433},   
+    {x: 1280, y: 0, width: 1280, height: 433},   
+    {x: 2560, y: 0, width: 1280, height: 433},   
+    {x: 3840, y: 0, width: 1280, height: 433},   
+    {x: 5120, y: 0, width: 1280, height: 433},   
+    {x: 6400, y: 0, width: 1280, height: 433},   
+    {x: 7680, y: 0, width: 1280, height: 433},   
+    {x: 8960, y: 0, width: 1280, height: 433},   
+    {x: 10240, y: 0, width: 1280, height: 433},  
+    {x: 11520, y: 0, width: 1280, height: 433},  
+    {x: 12800, y: 0, width: 1280, height: 433},  
+    {x: 14080, y: 0, width: 1280, height: 433},  
+    {x: 15360, y: 0, width: 1280, height: 433},  
+    {x: 16640, y: 0, width: 1280, height: 433},  
+    {x: 17920, y: 0, width: 1280, height: 433}    
+];
+
+
+
+/* img/backgrounds --> bg5 */
+let escenari_comunista = [
+    {x: 0, y: 0, width: 1280, height: 605},   
+    {x: 1280, y: 0, width: 1280, height: 605},   
+    {x: 2560, y: 0, width: 1280, height: 605},   
+    {x: 3840, y: 0, width: 1280, height: 605},   
+    {x: 5120, y: 0, width: 1280, height: 605}     
+];
+
+
+
+/* img/backgrounds --> bg6 */
+let escenari_elefantes = [
+    {x: 0, y: 0, width: 1280, height: 592},   
+    {x: 1280, y: 0, width: 1280, height: 592},   
+    {x: 2560, y: 0, width: 1280, height: 592},   
+    {x: 3840, y: 0, width: 1280, height: 592},   
+    {x: 5120, y: 0, width: 1280, height: 592},   
+    {x: 6400, y: 0, width: 1280, height: 592},   
+    {x: 7680, y: 0, width: 1280, height: 592},   
+    {x: 8960, y: 0, width: 1280, height: 592},   
+    {x: 10240, y: 0, width: 1280, height: 592}    
+];
+
+
+
+/* img/backgrounds --> bg7 */
+let escenari_barco = [
+    {x: 0, y: 0, width: 621,height: 224},   
+    {x: 621, y: 0, width: 621,height: 224},   
+    {x: 1242, y: 0, width: 621,height: 224},   
+    {x: 2484, y: 0, width: 621,height: 224}
+];
