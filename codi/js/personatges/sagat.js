158b47904f19d89c6eace1b9994f14cb3df34e9f
Sailok <127998789+Sailok25@users.noreply.github.com>
2025-03-31 22:12:13 +0200

    Delete codi directory

--- codi/js/personatges/sagat.js
+++
@@ -1,6 +0,0 @@
-let sagat_base = [
-    {x: 3, y: 788, width: 74,height: 117},
-    {x: 84, y: 786, width: 74, height: 119},
-    {x: 165, y: 785, width: 74,height: 120},
-    {x: 246, y: 790, width: 55,height: 115}
-];
